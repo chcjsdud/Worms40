06f437ba8df17a0fc433b2e024f3fb7cae903227
희상 황 <gmltkd0423@naver.com>
2022-05-17 15:58:36 +0900

    리소스 로드 업데이트

--- Worms_Armageddon/GameEngineBase/GameEngineCustomStringSet.h
+++ Worms_Armageddon/GameEngineBase/GameEngineCustomStringSet.h
@@ -3,6 +3,38 @@
 #define DIR_WORMS_PARENTS "Worms_Armageddon"
 #define DIR_RESOURCES "Resources"
 #define DIR_IMAGE "Image"
+#define DIR_EFFECTS "Effects"
+#define DIR_FONT "Font"
+#define DIR_MAP "Map"
+#define DIR_WEAPONS "Weapons"
+#define DIR_WORMS "Worms"
+#define DIR_MISC "Misc"
+#define DIR_UI "UI"
+
+
+//#define DIR_HIT "hit"
+//#define DIR_NOTUSED "notUsed"
+
+
+//#define DIR_AIM_TAGET "Aim_Taget"
+//#define DIR_CURSOR "Cursor"
+//#define DIR_GRAVE "Grave"
+//#define DIR_ITEM "Item"
+//#define DIR_PLAYER_ARROW "Player_arrow"
+
+
+//#define DIR_BUTTONBORDERS "ButtonBorders"
+//#define DIR_CREATETEAM "CreateTeam"
+//#define DIR_GAMEOPTIONS "gameoptions"
+//#define DIR_HPBAR "Hpbar"
+//#define DIR_MENU "Menu"
+//#define DIR_MULTIPLAY "Multiplay"
+//#define DIR_SELECTMAP "SelectMap"
+//#define DIR_TIMER "Timer"
+//#define DIR_WEAPON_ICONS "Weapon_icons"
+
+
+
 
 // 파일 확장자
 #define FILE_EXT_BMP "Bmp"
--- Worms_Armageddon/GameEngineContents/Worms.cpp
+++ Worms_Armageddon/GameEngineContents/Worms.cpp
@@ -44,8 +44,136 @@ void Worms::GameInit()
 
 	}
 
+	
+
+	//Effects폴더내 파일 로드
+	{
+		GameEngineDirectory Dir;
+		Dir.MoveParent(DIR_WORMS_PARENTS);
+		Dir.Move(DIR_RESOURCES);
+		Dir.Move(DIR_IMAGE);
+		Dir.Move(DIR_EFFECTS);
+
+		std::vector<GameEngineFile> AllImageFileList = Dir.GetAllFile(FILE_EXT_BMP);
+
+		for (size_t i = 0; i < AllImageFileList.size(); i++)
+		{
+			GameEngineImageManager::GetInst()->Load(AllImageFileList[i].GetFullPath());
+		}
+
+	}
 
 
+	//Font폴더내 파일 로드
+	{
+		GameEngineDirectory Dir;
+		Dir.MoveParent(DIR_WORMS_PARENTS);
+		Dir.Move(DIR_RESOURCES);
+		Dir.Move(DIR_IMAGE);
+		Dir.Move(DIR_FONT);
+
+		std::vector<GameEngineFile> AllImageFileList = Dir.GetAllFile(FILE_EXT_BMP);
+
+		for (size_t i = 0; i < AllImageFileList.size(); i++)
+		{
+			GameEngineImageManager::GetInst()->Load(AllImageFileList[i].GetFullPath());
+		}
+
+	}
+
+
+	//Map폴더내 파일 로드
+	{
+		GameEngineDirectory Dir;
+		Dir.MoveParent(DIR_WORMS_PARENTS);
+		Dir.Move(DIR_RESOURCES);
+		Dir.Move(DIR_IMAGE);
+		Dir.Move(DIR_MAP);
+
+		std::vector<GameEngineFile> AllImageFileList = Dir.GetAllFile(FILE_EXT_BMP);
+
+		for (size_t i = 0; i < AllImageFileList.size(); i++)
+		{
+			GameEngineImageManager::GetInst()->Load(AllImageFileList[i].GetFullPath());
+		}
+
+	}
+
+
+	//Misc폴더내 파일 로드
+	{
+		GameEngineDirectory Dir;
+		Dir.MoveParent(DIR_WORMS_PARENTS);
+		Dir.Move(DIR_RESOURCES);
+		Dir.Move(DIR_IMAGE);
+		Dir.Move(DIR_MISC);
+
+		std::vector<GameEngineFile> AllImageFileList = Dir.GetAllFile(FILE_EXT_BMP);
+
+		for (size_t i = 0; i < AllImageFileList.size(); i++)
+		{
+			GameEngineImageManager::GetInst()->Load(AllImageFileList[i].GetFullPath());
+		}
+
+	}
+
+
+
+	//UI폴더내 파일 로드
+	{
+		GameEngineDirectory Dir;
+		Dir.MoveParent(DIR_WORMS_PARENTS);
+		Dir.Move(DIR_RESOURCES);
+		Dir.Move(DIR_IMAGE);
+		Dir.Move(DIR_UI);
+
+		std::vector<GameEngineFile> AllImageFileList = Dir.GetAllFile(FILE_EXT_BMP);
+
+		for (size_t i = 0; i < AllImageFileList.size(); i++)
+		{
+			GameEngineImageManager::GetInst()->Load(AllImageFileList[i].GetFullPath());
+		}
+
+	}
+
+
+	//WEAPONS폴더내 파일 로드
+	{
+		GameEngineDirectory Dir;
+		Dir.MoveParent(DIR_WORMS_PARENTS);
+		Dir.Move(DIR_RESOURCES);
+		Dir.Move(DIR_IMAGE);
+		Dir.Move(DIR_WEAPONS);
+
+		std::vector<GameEngineFile> AllImageFileList = Dir.GetAllFile(FILE_EXT_BMP);
+
+		for (size_t i = 0; i < AllImageFileList.size(); i++)
+		{
+			GameEngineImageManager::GetInst()->Load(AllImageFileList[i].GetFullPath());
+		}
+
+	}
+
+
+	//WORMS폴더내 파일 로드
+	{
+		GameEngineDirectory Dir;
+		Dir.MoveParent(DIR_WORMS_PARENTS);
+		Dir.Move(DIR_RESOURCES);
+		Dir.Move(DIR_IMAGE);
+		Dir.Move(DIR_WORMS);
+
+		std::vector<GameEngineFile> AllImageFileList = Dir.GetAllFile(FILE_EXT_BMP);
+
+		for (size_t i = 0; i < AllImageFileList.size(); i++)
+		{
+			GameEngineImageManager::GetInst()->Load(AllImageFileList[i].GetFullPath());
+		}
+
+	}
+
+
+	
 	CreateLevel<TitleLevel>(LEVEL_TITLE_LEVEL);
 	CreateLevel<PlayLevel>(LEVEL_PLAY_LEVEL);
 	ChangeLevel(LEVEL_TITLE_LEVEL);
