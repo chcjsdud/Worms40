33f59321e70e5e95760ac8d4e05e24bc86a3165b
LeeJungmin <apple0220@naver.com>
2022-05-17 15:50:43 +0900

    박지영 : StartLogo액터 추가

---
+++ Worms_Armageddon/GameEngineContents/StartLogo.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <GameEngine/GameEngineActor.h>
+
+// Ό³Έν :
+class GameEngineRenderer;
+class StartLogo : public GameEngineActor
+{
+public:
+	// constrcuter destructer
+	StartLogo();
+	~StartLogo();
+
+	// delete Function
+	StartLogo(const StartLogo& _Other) = delete;
+	StartLogo(StartLogo&& _Other) noexcept = delete;
+	StartLogo& operator=(const StartLogo& _Other) = delete;
+	StartLogo& operator=(StartLogo&& _Other) noexcept = delete;
+
+protected:
+
+	virtual void Start() override;
+	virtual void Update() override;
+
+private:
+
+	GameEngineRenderer* StartLogoRenderer_;
+
+};
+
