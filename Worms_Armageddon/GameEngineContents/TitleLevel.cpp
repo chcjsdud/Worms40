017bc5cee397dd0e4da2d68a6460f7a35aaf4337
LeeJungmin <apple0220@naver.com>
2022-05-17 14:23:58 +0900

    이정민 : 디버그용 레벨 이동 키 추가(타이틀레벨)

--- Worms_Armageddon/GameEngineContents/TitleLevel.cpp
+++ Worms_Armageddon/GameEngineContents/TitleLevel.cpp
@@ -1,4 +1,7 @@
 #include "TitleLevel.h"
+#include <GameEngine/GameEngine.h>
+#include <GameEngine/GameEngineLevel.h>
+#include <GameEngineBase/GameEngineInput.h>
 
 TitleLevel::TitleLevel() 
 {
@@ -11,9 +14,24 @@ TitleLevel::~TitleLevel()
 
 void TitleLevel::Loading()
 {
+	// 디버그용
+	if (false == GameEngineInput::GetInst()->IsKey("MoveTitleLevel"))
+	{
+		GameEngineInput::GetInst()->CreateKey("MoveTitleLevel", '1');
+		GameEngineInput::GetInst()->CreateKey("MovePlayLevel", '2');
+	}
 }
 
 void TitleLevel::Update()
 {
+	// 디버그용
+	if (GameEngineInput::GetInst()->IsDown("MoveTitleLevel"))
+	{
+		GameEngine::GetInst().ChangeLevel("TitleLevel");
+	}
+	if (GameEngineInput::GetInst()->IsDown("MovePlayLevel"))
+	{
+		GameEngine::GetInst().ChangeLevel("PlayLevel");
+	}
 }
 
